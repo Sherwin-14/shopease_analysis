496d9fa34602c7290406d885a06e9593b60c2a88
Sherwin-14 <varghesesheriwn@gmail.com>
2024-08-18 13:27:47 +0530

    Added business problem

---
+++ pages/intro.py
@@ -0,0 +1,5 @@
+from utils import *
+
+st.title("Business Problem Statement")
+
+st.write("##### As the e-commerce market continues to grow, our company is facing increasing competition and pressure to optimize our online store to meet the evolving needs of our customers. Our stakeholders have identified a need for a comprehensive analysis of our e-commerce store to better understand our customer segments, their preferences, and the workflow of our entire store.")
